eb27597045ada3829f449b12a4e240700ffb9778
Mark Kornelsen <mark.kornelsen@libertymutual.com>
2018-02-13 09:37:49 -0500

    Adding spring-generated files

---
+++ src/main/java/com/example/contextloads/ContextloadsApplication.java
@@ -0,0 +1,12 @@
+package com.example.contextloads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextloadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextloadsApplication.class, args);
+	}
+}
